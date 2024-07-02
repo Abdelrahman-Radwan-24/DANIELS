434a752fee08b49f58939da7cee03d648a498f91
Abdelrahman-Radwan-24 <abdelrahmanradwan2024@gmail.com>
2024-07-02 03:12:19 +0300

    upload

---
+++ js/index.js
@@ -0,0 +1,26 @@
+// Typing-header //
+
+// var typed = new Typed(".auto-type", {
+//   strings: ["Larry Daniels", "Developer", "Designer"],
+//   typeSpeed: 50,
+//   backSpeed: 50,
+//   loop: true,
+// });
+
+// OWl-Carousel //
+
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    items: 1,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+  });
+});
+
+
+
+
+
+
+
